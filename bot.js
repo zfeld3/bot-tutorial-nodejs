ef533e22fb143b1387eeb5977b97b93ae1725dda
zfeld3 <zfeld3@users.noreply.github.com>
2016-11-19 14:08:12 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-  botRegex = '@coinFlip';
+  botRegex = /@coinFlip$/;
   
 
   if(request.text && botRegex.test(request.text)) {
