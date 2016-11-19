735390bdd31656c153cbe66a35cc9510c02ec079
zfeld3 <zfeld3@users.noreply.github.com>
2016-11-19 15:09:40 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,13 +6,18 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
   botRegex = /@coinFlip$/;
-  
+  var botRegex2 = /@randNum$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(400);
     coinFlip();
     this.res.end();
-  } else {
+  } else if( request.text && botRegex2.test(request.text)){
+    this.res.writeHead(400);
+    randNumber(10);
+    this.res.end();
+  }
+  else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
@@ -27,6 +32,42 @@ function coinFlip() {
   }else{
     botResponse = 'tails';
   }
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+
+function randNum(var upperLimit) {
+  var botResponse, options, body, botReq, userID, userNickname, randNumber;
+  randNumber = (Math.random() * (upperLimit -1)) + 1;
+  botResponse = Math.round(randNumber);
 
   options = {
     hostname: 'api.groupme.com',
