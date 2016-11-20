1ee59904bc5477beec58b5901fe5199ca79581b9
zfeld3 <zfeld3@users.noreply.github.com>
2016-11-19 19:22:33 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,7 @@ function respond() {
   var botRegex6 = /i'm hungry$/;
   var botRegex7 = /Im hungry$/;
   var botRegex8 = /im hungry$/;
+  var botRegex9 = /@bot help$/;
   
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(400);
@@ -34,6 +35,10 @@ function respond() {
   }else if(request.text && (botRegex5.test(request.text) || botRegex6.test(request.text) || botRegex7.test(request.text) || botRegex8.test(request.text))){
     this.res.writeHead(200);
     hiImHackathonBot();
+    this.res.end();
+  }else if(request.text && (botRegex9.test(request.text)){
+    this.res.writeHead(200);
+    help();
     this.res.end();
   }
   else {
@@ -185,6 +190,37 @@ function hiImHackathonBot() {
   });
   botReq.end(JSON.stringify(body));
 }
+function help() {
+  var botResponse, options, body, botReq, userID, userNickname;
+    botResponse = '"@bot random number 1 through 10" , "@bot random number 1 throuhg 100" , "@bot flip a coin" , "magic conch";
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
 
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
 
 exports.respond = respond;
