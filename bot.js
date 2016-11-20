0d6d5030d757a10db6dd7ad05a8e14a51218e5dd
zfeld3 <zfeld3@users.noreply.github.com>
2016-11-19 22:06:53 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,8 @@ function respond() {
     this.res.end();
   }else if(request.text && botRegex4.test(request.text)){
     this.res.writeHead(200);
-    magicConch();
+    gifAndWait();
+    window.setInterval(magicConch(), 500);
     this.res.end();
   }else if(request.text && (botRegex5.test(request.text) || botRegex6.test(request.text) || botRegex7.test(request.text) || botRegex8.test(request.text))){
     this.res.writeHead(200);
@@ -186,6 +187,37 @@ function hiImHackathonBot() {
   });
   botReq.end(JSON.stringify(body));
 }
+function gifAndWait() {
+  var botResponse, options, body, botReq, userID, userNickname;
+    botResponse = "http://makeagif.com/i/AAOe2o";
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
 
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
 
 exports.respond = respond;
