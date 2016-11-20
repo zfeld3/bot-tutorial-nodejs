6d90fcb471e6092467fc1f49f9796f911f91242c
zfeld3 <zfeld3@users.noreply.github.com>
2016-11-19 20:01:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -190,37 +190,6 @@ function hiImHackathonBot() {
   });
   botReq.end(JSON.stringify(body));
 }
-function help() {
-  var botResponse, options, body, botReq, userID, userNickname;
-    botResponse = '@bot random number through 10 , @bot random number 1 through 100 , @bot flip a coin , magic conch';
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
 
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
-}
 
 exports.respond = respond;
